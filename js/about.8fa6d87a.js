ae7cd75daee8584b801752130cec20cf395d7354
jofaval <jofaval@users.noreply.github.com>
2022-06-30 20:10:11 +0000

    Deploying to gh-pages from @ jofaval/gh-btc-vue-final@e68fb734b64356e5e6dc476f65115970c8fef45d 🚀

---
+++ js/about.8fa6d87a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgh_btc_vue_final"]=self["webpackChunkgh_btc_vue_final"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],l=n(1001),s={},c=(0,l.Z)(s,u,a,!1,null,null,null),r=c.exports}}]);
+//# sourceMappingURL=about.8fa6d87a.js.map
