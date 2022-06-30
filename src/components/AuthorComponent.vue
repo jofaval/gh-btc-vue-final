ab6a158e900fe1072f8d6b2309b214ef285cb79f
pepe_fabra_valverde <jofaval@outlook.com>
2022-06-30 20:09:50 +0200

    #15 Create AuthorComponent

---
+++ src/components/AuthorComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="author">
+    <AuthorProfile />
+    <AuthorBiography
+      bio="Software Engineer with Full Stack experience, currently working as a Frontend Developer."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AuthorBiography from "@/components/AuthorBiography.vue";
+import AuthorProfile from "@/components/AuthorProfile.vue";
+
+export default Vue.extend({
+  name: "AuthorView",
+  data() {
+    return {
+      authorImgSource: "@/assets/jofaval.jpg",
+    };
+  },
+  components: { AuthorBiography, AuthorProfile },
+});
+</script>
+
+<style scoped>
+.author {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
---
+++ tests/unit/AuthorComponent.spec.ts
@@ -0,0 +1,12 @@
+import { mount } from "@vue/test-utils";
+import AuthorComponent from "@/components/AuthorComponent.vue";
+
+describe("AuthorComponent", () => {
+  it("should render properly", () => {
+    const wrapper = mount(AuthorComponent);
+    const author = wrapper.find(".author");
+    expect(author.exists()).toBe(true);
+    expect(author.find(".author__profile").exists()).toBe(true);
+    expect(author.find(".author__biography").exists()).toBe(true);
+  });
+});
