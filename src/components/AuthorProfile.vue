cc624e71fa7292e5cfc8c575d4e619ba075df60c
pepe_fabra_valverde <jofaval@outlook.com>
2022-06-30 19:50:15 +0200

    #14 Create AuthorProfile component

---
+++ src/components/AuthorProfile.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      alt: "no-image",
+      title: "Author profile image",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="author__profile">
+    <img
+      class="author__profile__img"
+      src="@/assets/jofaval.jpg"
+      :alt="title"
+      :title="title"
+    />
+  </div>
+</template>
+
+<style>
+:root {
+  --img-size: 200px;
+}
+
+.author__profile__img {
+  border-radius: 2.5rem;
+  max-width: var(--img-size);
+  max-height: var(--img-size);
+  min-width: var(--img-size);
+  min-height: var(--img-size);
+}
+</style>
---
+++ tests/unit/AuthorProfile.spec.ts
@@ -0,0 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
+import AuthorProfile from "@/components/AuthorProfile.vue";
+
+describe("AuthorProfile", () => {
+  it("should render properly", () => {
+    const wrapper = shallowMount(AuthorProfile);
+    expect(wrapper.find(".author__profile").exists()).toBe(true);
+    const paragraph = wrapper.find("img.author__profile__img");
+    expect(paragraph.exists()).toBe(true);
+    expect(paragraph.attributes()).toEqual(
+      expect.objectContaining({
+        alt: "Author profile image",
+        class: "author__profile__img",
+        src: "@/assets/jofaval.jpg",
+        title: "Author profile image",
+      })
+    );
+  });
+});
