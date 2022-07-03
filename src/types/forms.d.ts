051c1f77295250863253b33c5a94c812473f73e7
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 01:11:10 +0200

    #61 Create contact store module

---
+++ src/types/forms.d.ts
@@ -0,0 +1,8 @@
+export type ContactFormType = {
+  fullName: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+export type ContactFormField = "fullName" | "email" | "subject" | "message";
