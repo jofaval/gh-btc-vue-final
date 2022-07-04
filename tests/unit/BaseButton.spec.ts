2d41b8b78d32611b955f9c0302f5977b12e9df59
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 09:09:07 +0200

    #9 Create BaseButtonComponent

---
+++ tests/unit/BaseButton.spec.ts
@@ -0,0 +1,21 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import BaseButton from "@/components/BaseButton.vue";
+
+describe("BaseButton", () => {
+  it("should render properly", () => {
+    const wrapper = shallowMount(BaseButton);
+    expect(wrapper.find(".base-button__container").exists()).toBe(true);
+    const baseButton = wrapper.find(".base-button");
+    expect(baseButton.exists()).toBe(true);
+    expect(baseButton.attributes("type")).toBe("submit");
+  });
+
+  it("should render as disabled", () => {
+    const wrapper = mount(BaseButton, {
+      propsData: {
+        disabled: true,
+      },
+    });
+    expect(wrapper.find(".base-button").attributes("disabled")).toBe("true");
+  });
+});
