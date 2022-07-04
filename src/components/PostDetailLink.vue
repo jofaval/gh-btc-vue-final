b10aad0d8a03534225e5a7048031779db5dc0d12
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 17:22:20 +0200

    #30 Create PostDetailLink

---
+++ src/components/PostDetailLink.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+// Vendors
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    post: { type: Object, required: true },
+    short: { type: Boolean, default: false },
+  },
+});
+</script>
+
+<template>
+  <router-link
+    :to="{ name: 'Detail', params: { postId: post.id } }"
+    :class="`post-detail__link ${short ? 'short' : ''}`"
+    >{{ post.title }}</router-link
+  >
+</template>
+
+<style scoped>
+.post-detail__link.short {
+  display: inline-block;
+  max-width: 10rem;
+  overflow-x: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.post-detail__link {
+  color: var(--tertiary-text-color);
+}
+</style>
---
+++ tests/unit/PostDetailLink.spec.ts
@@ -0,0 +1,34 @@
+// Vendors
+import { shallowMount } from "@vue/test-utils";
+// Components
+import PostDetailLink from "@/components/PostDetailLink.vue";
+
+describe("PostDetailLink", () => {
+  const post = {
+    id: 1,
+    userId: 1,
+    title: "1",
+    body: "1",
+  };
+
+  it("should render properly", () => {
+    const wrapper = shallowMount(PostDetailLink, {
+      propsData: {
+        post,
+      },
+    });
+    expect(wrapper.find("router-link.post-detail__link").exists()).toBe(true);
+  });
+
+  it("should render with the short attribute", () => {
+    const wrapper = shallowMount(PostDetailLink, {
+      propsData: {
+        post,
+        short: true,
+      },
+    });
+    expect(wrapper.find("router-link.post-detail__link.short").exists()).toBe(
+      true
+    );
+  });
+});
