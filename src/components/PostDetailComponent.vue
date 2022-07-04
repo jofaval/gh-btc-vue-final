85c19beca4345dd8f5837c5c4d998b0d4938ad3f
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 19:00:58 +0200

    #17 Create PostDetailsComponent

---
+++ src/components/PostDetailComponent.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+// Vendors
+import Vue from "vue";
+// Components
+import UserLink from "./UserLink.vue";
+
+export default Vue.extend({
+  props: {
+    post: { type: Object, required: true },
+    user: { type: Object, required: true },
+  },
+  components: { UserLink },
+  data() {
+    return {
+      show: true,
+    };
+  },
+  mounted() {
+    this.shouldShow();
+  },
+  watch: {
+    post() {
+      this.shouldShow();
+    },
+  },
+  methods: {
+    shouldShow() {
+      this.show = Boolean(this.post) && Boolean(this.user);
+    },
+  },
+});
+</script>
+
+<template>
+  <Transition>
+    <div class="post-detail" v-if="show">
+      <h1 class="post-detail__title h1">{{ post.title }}</h1>
+      <h2 class="post-detail__subtitle post-detail__author h5" v-if="user">
+        Created by: <UserLink :user="user" />
+      </h2>
+
+      <p class="post-detail__body">{{ post.body }}</p>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.post-detail__title {
+  color: var(--tertiary-text-color);
+  font-weight: bold;
+}
+
+.post-detail__body {
+  max-width: 800px;
+  padding: 2rem;
+  margin: auto;
+}
+</style>
