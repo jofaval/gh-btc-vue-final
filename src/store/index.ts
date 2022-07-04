62bc779e2cdd27a36afca74ae5292512c8c04c3f
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 21:03:39 +0200

    Add index as exports of the subpackages

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,3 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import store from "./store";
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+export default store;
