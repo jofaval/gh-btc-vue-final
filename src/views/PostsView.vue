92ccebe9d84d826fa35d1e7a0eeda1e402d390f9
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 19:27:24 +0200

    #29 Create all posts page and component

---
+++ src/views/PostsView.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+// Vendors
+import Vue from "vue";
+// Store
+import store from "@/store";
+// Components
+import PostsComponent from "@/components/PostsComponent.vue";
+// Types
+import type { PostType } from "@/types/post";
+
+export default Vue.extend({
+  name: "PostsView",
+  components: { PostsComponent },
+  computed: {
+    posts(): PostType[] {
+      return store.getters.posts();
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="page__posts w-100 h-100">
+    <PostsComponent :posts="posts" />
+  </div>
+</template>
+
+<style scoped>
+.page__posts {
+  display: flex;
+  flex-direction: column;
+}
+</style>
