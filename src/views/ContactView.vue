76c6c341d257e5b168c22062f592d8e99821a621
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 20:50:01 +0200

    #12 Create contact page

---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+// Vendors
+import Vue from "vue";
+// Components
+import FormularyComponent from "@/components/FormularyComponent.vue";
+
+export default Vue.extend({
+  name: "ContactView",
+  components: { FormularyComponent },
+});
+</script>
+
+<template>
+  <div class="contact w-100 pt-sm-4 rounded">
+    <FormularyComponent />
+  </div>
+</template>
+
+<style scoped></style>
