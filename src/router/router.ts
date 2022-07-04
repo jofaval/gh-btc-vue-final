bd3c9c5c5cfd196c8d1ef90ffb944375ef2f9c6f
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 20:52:53 +0200

    #63 Create and implement the routing system

---
+++ src/router/router.ts
@@ -0,0 +1,27 @@
+// Vendors
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+// Utilities
+import { changeTitle } from "@/utils/browser.utils";
+import { capitalize } from "@/utils/string.utils";
+// Constants
+import routerConstants from "./constants/router.constants";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = routerConstants.ROUTES;
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.afterEach((to) => {
+  Vue.nextTick(() => {
+    if (!to?.meta?.title) return;
+    changeTitle(capitalize(to.meta.title));
+  });
+});
+
+export default router;
---
+++ src/types/router.d.ts
@@ -0,0 +1,12 @@
+type RouterType = {
+  /** The name of the route */
+  name: string;
+  /** The target path */
+  path: string;
+  /** The component it will load */
+  component?: Element;
+  /** The route it will redirect to */
+  redirect: string;
+  /** The meta values, such as the title of the document */
+  meta: { title: string };
+};
