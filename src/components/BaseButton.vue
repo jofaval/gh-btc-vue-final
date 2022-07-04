2c920e89f3c9e0a92c79bf70c5ab468d727cfea6
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 09:09:07 +0200

    #9 Create BaseButtonComponent

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+// Vendors
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    label: { type: String, default: "Submit" },
+    variant: { type: String, default: "primary" },
+    disabled: { type: Boolean, default: false },
+    disabledText: { type: String, default: "" },
+  },
+  computed: {
+    display(): boolean {
+      // Disabled for this project's purpose, for the moment being
+      return this.disabled && !!this.disabledText && false;
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="base-button__container btn">
+    <b-button
+      class="base-button"
+      type="submit"
+      :variant="`outline-${variant}`"
+      :disabled="disabled"
+      >{{ label }}</b-button
+    >
+    <small v-if="display" class="base-button__desc">{{ disabledText }}</small>
+  </div>
+</template>
+
+<style scoped>
+.base-button__container {
+  display: flex;
+  flex-direction: column;
+  color: var(--primary-text-color);
+}
+</style>
