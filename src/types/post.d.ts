c9db96b05e4fd103c6914eb756750b947394414c
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 08:22:31 +0200

    #58 Create placeholder store module

---
+++ src/types/post.d.ts
@@ -0,0 +1,10 @@
+export type PostType = {
+  /** The author ID, numeric format */
+  userId: number;
+  /** The post ID, numeric format */
+  id: number;
+  /** The post title */
+  title: string;
+  /** The post content */
+  body: string;
+};
