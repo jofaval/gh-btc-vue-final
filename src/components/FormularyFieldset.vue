08cc2db526b15da18b842e38d6205504a0adac94
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 20:43:45 +0200

    #32 Create FormularyFieldsetComponent

---
+++ src/components/FormularyFieldset.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+// Vendors
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    legend: { type: String, required: true },
+  },
+});
+</script>
+
+<template>
+  <fieldset class="formulary__fieldset p-3 my-3 rounded">
+    <span class="formulary__fieldset__legend legend">{{ legend }}</span>
+    <slot />
+  </fieldset>
+</template>
+
+<style scoped>
+.formulary__fieldset {
+  /* border: thin solid rgba(175, 175, 175, 0.2); */
+  box-shadow: 0 0.5rem 1rem var(--glow-shadow);
+  position: relative;
+}
+
+.formulary__fieldset__legend {
+  position: absolute;
+  top: -0.5rem;
+  font-size: 1.15em;
+  color: var(--tertiary-text-color);
+}
+</style>
