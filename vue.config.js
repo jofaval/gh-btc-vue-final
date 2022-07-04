241799e2b57a241636da9a0fbe7bb5c513d0deda
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-04 09:21:38 +0200

    Configure public path for production deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 
+const publicPath =
+  process.env.NODE_ENV === "production" ? "/gh-btc-vue-final" : "/";
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath,
 });
